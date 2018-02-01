b52172a45c8157acc21e9ab3a7ae2db38d057733
Tamas Szabo <szabo.tamas.mk@gmail.com>
2018-02-01 22:54:00 +0100

    thenAnswer

--- src/test/java/HelloWorldTestMockito.java
+++ src/test/java/HelloWorldTestMockito.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -27,5 +30,23 @@ public class HelloWorldTestMockito {
         assertEquals("Hello World!", result );
         verify(helloWorld);
 
+    }
+    
+    @Test
+    public void testThenAnswer() {
+        HelloWorldService helloWorldService = new HelloWorldService();
+
+        Mockito.when(helloWorld.printHelloWorld()).thenAnswer(new Answer() {
+            public int count = 0;
+            public String[] names = {"Hello User1", "Hello User2", "Hello User3"};
+
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                return names[count++];
+            }
+        });
+
+        assertEquals("Hello User1", helloWorldService.getHelloWord(helloWorld));
+        assertEquals("Hello User2", helloWorldService.getHelloWord(helloWorld));
+        assertEquals("Hello User3", helloWorldService.getHelloWord(helloWorld));
     }
 }
